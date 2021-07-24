9889f23b346cc7262ffd31c84c30f20d5d8ef743
Kaleb-Bruwer <u19064242@tuks.co.za>
2021-07-24 21:49:12 +0200

    Additional test for PacketReader::append

--- Test/Protocol/PacketReaderTest.cpp
+++ Test/Protocol/PacketReaderTest.cpp
@@ -28,7 +28,39 @@ TEST(PacketReader, append){
     ASSERT_TRUE(b == 67438087);
 
     int c = reader.readInt();
-    cout << c << endl;
     ASSERT_TRUE(c == 134810123);
+    ASSERT_TRUE(reader.reachedEnd());
+}
+
+TEST(PacketReader, append2){
+    const int dataSize = 8;
+    char data[dataSize];
+    for(int i=0; i<dataSize; i++){
+        data[i] = i;
+    }
+
+    PacketReader reader(&(data[0]), dataSize);
+
+
+    const int data2Size = 8;
+    char data2[data2Size];
+    for(int i=0; i<data2Size; i++){
+        data2[i] = i + dataSize;
+    }
+
+    int a = reader.readInt();
+    ASSERT_TRUE(a == 66051);
+
+    reader.append(&(data2[0]), data2Size);
+
+
+    int b = reader.readInt();
+    ASSERT_TRUE(b == 67438087);
+
+    int c = reader.readInt();
+    ASSERT_TRUE(c == 134810123);
+    ASSERT_FALSE(reader.reachedEnd());
+
+    reader.readInt();
     ASSERT_TRUE(reader.reachedEnd());
 }
