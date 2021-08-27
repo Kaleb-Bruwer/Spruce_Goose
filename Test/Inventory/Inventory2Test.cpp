2b99a3c26816e2d7a636de056245cc116ad121fc
Kaleb-Bruwer <u19064242@tuks.co.za>
2021-08-27 19:18:47 +0200

    unit tests for shift click

--- Test/Inventory/Inventory2Test.cpp
+++ Test/Inventory/Inventory2Test.cpp
@@ -16,10 +16,11 @@ protected:
 
     };
 
-    ClickWindowJob* initJob(int slotNum = 9){
+    ClickWindowJob* initJob(int mode = 0, int slotNum = 9){
         ClickWindowJob* result = new ClickWindowJob();
 
         result->windowID = 0;
+        result->mode = mode;
         result->slotNum = slotNum;
 
         return result;
@@ -40,7 +41,7 @@ protected:
     };
 
     void testMode0Btn0(Slot s, Slot h, int slot = 9){
-        ClickWindowJob* job = initJob(slot);
+        ClickWindowJob* job = initJob(0, slot);
         job->mode = 0;
         job->button = 0;
 
@@ -54,7 +55,7 @@ protected:
     }
 
     void testMode0Btn1(Slot s, Slot h, int slot = 9){
-        ClickWindowJob* job = initJob(slot);
+        ClickWindowJob* job = initJob(0, slot);
         job->mode = 0;
         job->button = 1;
 
@@ -104,6 +105,7 @@ protected:
         ASSERT_TRUE(inventory2.hover == hExpect) << "hover: " << inventory2.hover << ", expected: " << hExpect;
         ASSERT_TRUE(inventory2.slots[slot] == sExpect) << "slot: " << inventory2.slots[slot] << ", expected: " << sExpect;
     }
+
 };
 
 // Single item
@@ -139,8 +141,7 @@ TEST_F(Inventory2Test, testMode0Btn0T7){
     Slot h = getStone();
     int slot = 0;
 
-    ClickWindowJob* job = initJob(slot);
-    job->mode = 0;
+    ClickWindowJob* job = initJob(0, slot);
     job->button = 0;
 
     inventory2.slots[slot] = s;
@@ -157,8 +158,7 @@ TEST_F(Inventory2Test, testMode0Btn0T8){
     Slot h = Slot();
     int slot = 0;
 
-    ClickWindowJob* job = initJob(slot);
-    job->mode = 0;
+    ClickWindowJob* job = initJob(0, slot);
     job->button = 0;
 
     inventory2.slots[slot] = s;
@@ -239,4 +239,250 @@ TEST_F(Inventory2Test, testMode0Btn1T13){
 
 TEST_F(Inventory2Test, testMode0Btn1T14){
     testMode0Btn1(getDirt(30), getStone(64));
+}
+
+
+// shift clicks
+
+// inventory to empty hotbar
+TEST_F(Inventory2Test, testMode1T1){
+    // Set up inventory
+    inventory2.slots[9] = getStone();
+
+    // Create job
+    ClickWindowJob* job = initJob(1);
+
+    // execute
+    inventory2.clickWindow(job, 0, altered, false);
+
+    Slot hExpect = Slot();
+    Slot sExpect1 = Slot();
+    Slot sExpect2 = getStone();
+
+    // Assertions
+    ASSERT_TRUE(inventory2.hover == hExpect) << "hover: " << inventory2.hover << ", expected: " << hExpect;
+    ASSERT_TRUE(inventory2.slots[9] == sExpect1) << "slot9: " << inventory2.slots[9] << ", expected: " << sExpect1;
+    ASSERT_TRUE(inventory2.slots[36] == sExpect2) << "slot36: " << inventory2.slots[36] << ", expected: " << sExpect2;
+
+}
+
+// hotbar to empty inventory
+TEST_F(Inventory2Test, testMode1T2){
+    // Set up inventory
+    inventory2.slots[36] = getStone();
+
+    // Create job
+    ClickWindowJob* job = initJob(1, 36);
+
+    // execute
+    inventory2.clickWindow(job, 0, altered, false);
+
+    Slot hExpect = Slot();
+    Slot sExpect1 = Slot();
+    Slot sExpect2 = getStone();
+
+    // Assertions
+    ASSERT_TRUE(inventory2.hover == hExpect) << "hover: " << inventory2.hover << ", expected: " << hExpect;
+    ASSERT_TRUE(inventory2.slots[36] == sExpect1) << "slot36: " << inventory2.slots[36] << ", expected: " << sExpect1;
+    ASSERT_TRUE(inventory2.slots[9] == sExpect2) << "slot9: " << inventory2.slots[9] << ", expected: " << sExpect2;
+}
+
+// Hotbar to inventory with dirt in 1st slot
+TEST_F(Inventory2Test, testMode1T3){
+    // Set up inventory
+    inventory2.slots[36] = getStone();
+    inventory2.slots[9] = getDirt();
+
+    // Create job
+    ClickWindowJob* job = initJob(1, 36);
+
+    // execute
+    inventory2.clickWindow(job, 0, altered, false);
+
+    Slot hExpect = Slot();
+    Slot sExpect1 = Slot();
+    Slot sExpect2 = getDirt();
+    Slot sExpect3 = getStone();
+
+    // Assertions
+    ASSERT_TRUE(inventory2.hover == hExpect) << "hover: " << inventory2.hover << ", expected: " << hExpect;
+    ASSERT_TRUE(inventory2.slots[36] == sExpect1) << "slot36: " << inventory2.slots[36] << ", expected: " << sExpect1;
+    ASSERT_TRUE(inventory2.slots[9] == sExpect2) << "slot9: " << inventory2.slots[9] << ", expected: " << sExpect2;
+    ASSERT_TRUE(inventory2.slots[10] == sExpect3) << "slot10: " << inventory2.slots[10] << ", expected: " << sExpect3;
+}
+
+// Hotbar to inventory that has a partial stack
+TEST_F(Inventory2Test, testMode1T4){
+    // Set up inventory
+    inventory2.slots[36] = getStone();
+    inventory2.slots[14] = getStone();
+
+    // Create job
+    ClickWindowJob* job = initJob(1, 36);
+
+    // execute
+    inventory2.clickWindow(job, 0, altered, false);
+
+    Slot hExpect = Slot();
+    Slot sExpect1 = Slot();
+    Slot sExpect2 = Slot();
+    Slot sExpect3 = getStone(2);
+
+    // Assertions
+    ASSERT_TRUE(inventory2.hover == hExpect) << "hover: " << inventory2.hover << ", expected: " << hExpect;
+    ASSERT_TRUE(inventory2.slots[36] == sExpect1) << "slot36: " << inventory2.slots[36] << ", expected: " << sExpect1;
+    ASSERT_TRUE(inventory2.slots[9] == sExpect2) << "slot9: " << inventory2.slots[9] << ", expected: " << sExpect2;
+    ASSERT_TRUE(inventory2.slots[14] == sExpect3) << "slot14: " << inventory2.slots[14] << ", expected: " << sExpect3;
+}
+
+// Hotbar to inventory that has a full stack
+TEST_F(Inventory2Test, testMode1T5){
+    // Set up inventory
+    inventory2.slots[36] = getStone();
+    inventory2.slots[14] = getStone(64);
+
+    // Create job
+    ClickWindowJob* job = initJob(1, 36);
+
+    // execute
+    inventory2.clickWindow(job, 0, altered, false);
+
+    Slot hExpect = Slot();
+    Slot sExpect1 = Slot();
+    Slot sExpect2 = getStone();
+    Slot sExpect3 = getStone(64);
+
+    // Assertions
+    ASSERT_TRUE(inventory2.hover == hExpect) << "hover: " << inventory2.hover << ", expected: " << hExpect;
+    ASSERT_TRUE(inventory2.slots[36] == sExpect1) << "slot36: " << inventory2.slots[36] << ", expected: " << sExpect1;
+    ASSERT_TRUE(inventory2.slots[9] == sExpect2) << "slot9: " << inventory2.slots[9] << ", expected: " << sExpect2;
+    ASSERT_TRUE(inventory2.slots[14] == sExpect3) << "slot14: " << inventory2.slots[14] << ", expected: " << sExpect3;
+}
+
+// Hotbar to inventory that has a full stack
+TEST_F(Inventory2Test, testMode1T6){
+    // Set up inventory
+    inventory2.slots[36] = getStone();
+    inventory2.slots[14] = getStone(64);
+    inventory2.slots[12] = getStone(4);
+
+    // Create job
+    ClickWindowJob* job = initJob(1, 36);
+
+    // execute
+    inventory2.clickWindow(job, 0, altered, false);
+
+    Slot hExpect = Slot();
+    Slot sExpect1 = Slot();
+    Slot sExpect2 = getStone(5);
+    Slot sExpect3 = getStone(64);
+
+    // Assertions
+    ASSERT_TRUE(inventory2.hover == hExpect) << "hover: " << inventory2.hover << ", expected: " << hExpect;
+    ASSERT_TRUE(inventory2.slots[36] == sExpect1) << "slot36: " << inventory2.slots[36] << ", expected: " << sExpect1;
+    ASSERT_TRUE(inventory2.slots[12] == sExpect2) << "slot12: " << inventory2.slots[12] << ", expected: " << sExpect2;
+    ASSERT_TRUE(inventory2.slots[14] == sExpect3) << "slot14: " << inventory2.slots[14] << ", expected: " << sExpect3;
+}
+
+// Hotbar to inventory that has an almost-full stack
+TEST_F(Inventory2Test, testMode1T7){
+    // Set up inventory
+    inventory2.slots[36] = getStone(8);
+    inventory2.slots[12] = getStone(60);
+    inventory2.slots[14] = getStone();
+
+    // Create job
+    ClickWindowJob* job = initJob(1, 36);
+
+    // execute
+    inventory2.clickWindow(job, 0, altered, false);
+
+    Slot hExpect = Slot();
+    Slot sExpect1 = Slot();
+    Slot sExpect2 = getStone(64);
+    Slot sExpect3 = getStone(5);
+
+    // Assertions
+    ASSERT_TRUE(inventory2.hover == hExpect) << "hover: " << inventory2.hover << ", expected: " << hExpect;
+    ASSERT_TRUE(inventory2.slots[36] == sExpect1) << "slot36: " << inventory2.slots[36] << ", expected: " << sExpect1;
+    ASSERT_TRUE(inventory2.slots[12] == sExpect2) << "slot12: " << inventory2.slots[12] << ", expected: " << sExpect2;
+    ASSERT_TRUE(inventory2.slots[14] == sExpect3) << "slot14: " << inventory2.slots[14] << ", expected: " << sExpect3;
+}
+
+TEST_F(Inventory2Test, testMode1T8){
+    // Set up inventory
+    inventory2.slots[36] = getStone();
+
+    for(int i=9; i < 36; i++){
+        inventory2.slots[i] = getDirt();
+    }
+
+    // Create job
+    ClickWindowJob* job = initJob(1, 36);
+
+    // execute
+    inventory2.clickWindow(job, 0, altered, false);
+
+    Slot hExpect = Slot();
+    Slot sExpect1 = getStone();
+    Slot sExpect2 = getDirt();
+
+    // Assertions
+    ASSERT_TRUE(inventory2.hover == hExpect) << "hover: " << inventory2.hover << ", expected: " << hExpect;
+    ASSERT_TRUE(inventory2.slots[36] == sExpect1) << "slot36: " << inventory2.slots[36] << ", expected: " << sExpect1;
+
+    for(int i=9; i<36; i++){
+        ASSERT_TRUE(inventory2.slots[i] == sExpect2) << "slot: " << inventory2.slots[i] << ", expected: " << sExpect2;
+    }
+}
+
+TEST_F(Inventory2Test, testMode1T9){
+    // Set up inventory
+    inventory2.slots[9] = getStone();
+
+    for(int i=36; i < 45; i++){
+        inventory2.slots[i] = getDirt();
+    }
+
+    // Create job
+    ClickWindowJob* job = initJob(1, 9);
+
+    // execute
+    inventory2.clickWindow(job, 0, altered, false);
+
+    Slot hExpect = Slot();
+    Slot sExpect1 = getStone();
+    Slot sExpect2 = getDirt();
+
+    // Assertions
+    ASSERT_TRUE(inventory2.hover == hExpect) << "hover: " << inventory2.hover << ", expected: " << hExpect;
+    ASSERT_TRUE(inventory2.slots[9] == sExpect1) << "slot9: " << inventory2.slots[9] << ", expected: " << sExpect1;
+
+    for(int i=36; i<45; i++){
+        ASSERT_TRUE(inventory2.slots[i] == sExpect2) << "slot: " << inventory2.slots[i] << ", expected: " << sExpect2;
+    }
+}
+
+// Hotbar to inventory that has a full stack
+TEST_F(Inventory2Test, testMode1T10){
+    // Set up inventory
+    inventory2.slots[36] = getStone(4);
+    inventory2.slots[14] = getStone(62);
+
+    // Create job
+    ClickWindowJob* job = initJob(1, 36);
+
+    // execute
+    inventory2.clickWindow(job, 0, altered, false);
+
+    Slot hExpect = Slot();
+    Slot sExpect1 = Slot();
+    Slot sExpect2 = getStone(2);
+    Slot sExpect3 = getStone(64);
+
+    // Assertions
+    ASSERT_TRUE(inventory2.hover == hExpect) << "hover: " << inventory2.hover << ", expected: " << hExpect;
+    ASSERT_TRUE(inventory2.slots[36] == sExpect1) << "slot36: " << inventory2.slots[36] << ", expected: " << sExpect1;
+    ASSERT_TRUE(inventory2.slots[9] == sExpect2) << "slot9: " << inventory2.slots[9] << ", expected: " << sExpect2;
+    ASSERT_TRUE(inventory2.slots[14] == sExpect3) << "slot14: " << inventory2.slots[14] << ", expected: " << sExpect3;
 }
