efcf806e03f36ddcfd1be7aa40e55bd7f10e0c0f
Kaleb-Bruwer <u19064242@tuks.co.za>
2021-08-27 15:20:55 +0200

    Some unit tests

--- Test/Inventory/Inventory2Test.cpp
+++ Test/Inventory/Inventory2Test.cpp
@@ -4,6 +4,9 @@
 
 class Inventory2Test : public ::testing::Test{
 protected:
+    AlteredSlots altered;
+    Inventory2 inventory2;
+
     virtual void SetUp(){
 
     };
@@ -11,8 +14,50 @@ protected:
     virtual void TearDown(){
 
     };
+
+    ClickWindowJob* initJob(){
+        ClickWindowJob* result = new ClickWindowJob();
+
+        result->windowID = 0;
+        result->slotNum = 9;
+
+        return result;
+    };
+
+    Slot getStone(){
+        Slot result;
+        result.itemID = 1;
+        result.itemCount = 1;
+        return result;
+    };
 };
 
-TEST_F(Inventory2Test, test1){
-    ASSERT_TRUE(true);
+TEST_F(Inventory2Test, simplePickup){
+    ClickWindowJob* job = initJob();
+    job->mode = 0;
+    job->button = 0;
+
+    inventory2.slots[9] = getStone();
+
+    inventory2.clickWindow(job, 0, altered, false);
+
+
+
+    ASSERT_TRUE(inventory2.hover == getStone()) << "Incorrect item";
+    ASSERT_TRUE(inventory2.slots[9] == Slot()) << "Incorrect item";
+}
+
+TEST_F(Inventory2Test, simplePutdown){
+    ClickWindowJob* job = initJob();
+    job->mode = 0;
+    job->button = 0;
+
+    inventory2.hover = getStone();
+
+    inventory2.clickWindow(job, 0, altered, false);
+
+
+
+    ASSERT_TRUE(inventory2.hover == Slot()) << "Incorrect item";
+    ASSERT_TRUE(inventory2.slots[9] == getStone()) << "Incorrect item";
 }
