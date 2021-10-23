ca48e3097a7f5421370ab759ed8b5c982312bc5f
Kaleb-Bruwer <u19064242@tuks.co.za>
2021-10-23 20:07:35 +0200

    Read recipes.nbt

--- src/Inventory/Crafting/Crafting.cpp
+++ src/Inventory/Crafting/Crafting.cpp
@@ -29,44 +29,59 @@ vector<Recipe> Crafting::getAllRecipes(){
 }
 
 void Crafting::readFromFile(){
-    string filename = "../Data/ShapedRecipes.txt";
+    // !!!!!!!!!!!!!!!!!!!!! OLD START !!!!!!!!!!!!!!!!!!!!!!!!!
+
+    // string filename = "../Data/ShapedRecipes.txt";
+    // ifstream file(filename);
+    // string line;
+    //
+    // int numRecipes = 0;
+    //
+    // if(!getline(file,line)){
+    //     //Empty file
+    //     cout << filename << " is empty or doesn't exist\n";
+    //     return;
+    // }
+    // else{
+    //     numRecipes = stoi(line);
+    //
+    //     for(int i=0; i<numRecipes; i++){
+    //         getline(file, line);
+    //         recipes.insert(ShapedRecipe(line));
+    //     }
+    // }
+    // file.close();
+    //
+    // filename = "../Data/UnshapedRecipes.txt";
+    // ifstream file2(filename);
+    // int numShapeless;
+    //
+    // if(!getline(file2, line)){
+    //     cout << filename << " is empty or doesn't exist";
+    // }
+    // else{
+    //     numShapeless = stoi(line);
+    //
+    //     for(int i = numRecipes; i < numRecipes + numShapeless; i++){
+    //         getline(file2, line);
+    //         recipes.insert(Recipe(line));
+    //     }
+    // }
+    //
+    // cout << "Loaded " << numRecipes + numShapeless << " crafting recipes\n";
+
+    // !!!!!!!!!!!!!!!!!!!!! OLD END !!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    string filename = "../Data/recipes.nbt";
     ifstream file(filename);
-    string line;
 
-    int numRecipes = 0;
+    char* array = new char[64000];
+    file.read(array, 64000);
 
-    if(!getline(file,line)){
-        //Empty file
-        cout << filename << " is empty or doesn't exist\n";
-        return;
-    }
-    else{
-        numRecipes = stoi(line);
-
-        for(int i=0; i<numRecipes; i++){
-            getline(file, line);
-            recipes.insert(ShapedRecipe(line));
-        }
-    }
+    int index =0;
+    Tag_Compound tc(array+1,index);
+    // cout << tc.printableStr() << endl;
     file.close();
-
-    filename = "../Data/UnshapedRecipes.txt";
-    ifstream file2(filename);
-    int numShapeless;
-
-    if(!getline(file2, line)){
-        cout << filename << " is empty or doesn't exist";
-    }
-    else{
-        numShapeless = stoi(line);
-
-        for(int i = numRecipes; i < numRecipes + numShapeless; i++){
-            getline(file2, line);
-            recipes.insert(Recipe(line));
-        }
-    }
-
-    cout << "Loaded " << numRecipes + numShapeless << " crafting recipes\n";
 }
 
 
