3fabd9bfb653d7f66acb51e6f4b6b1c1b05cba22
Kaleb-Bruwer <u19064242@tuks.co.za>
2022-02-15 14:45:56 +0200

    Octree tested

--- Test/testMain.cpp
+++ Test/testMain.cpp
@@ -15,10 +15,10 @@
 
 #include "./General/FuelTableTest.cpp"
 
-#include "./Datastructures/Octree/OctreeTest.cpp"
 #include "./Datastructures/Octree/OctLeafTest.cpp"
 #include "./Datastructures/Octree/OctInternalTest.cpp"
 #include "./Datastructures/Octree/OctRootTest.cpp"
+#include "./Datastructures/Octree/OctreeTest.cpp"
 
 int main(int argc, char **argv){
     testing::InitGoogleTest(&argc, argv);
