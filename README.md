cf10614f9a4b6717b62f9c8673b54a862a2f95a4
Kaleb-Bruwer <kaleb.bruwer@gmail.com>
2022-09-16 11:41:19 +0200

    Renamed to Spruce Goose

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Widecraft
+# Spruce Goose
 
 An attempt at creating a more scalable minecraft server, with the goal of hosting thousands of players concurrently in the same world.
--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,6 @@ using namespace std;
 
 int main(){
 
-    Server widecraftServer(25565); //It all happens in constructor
+    Server spruceGoose(25565); //It all happens in constructor
     cout << "main() exits\n";
-
-
-    /*
-    double a = 1023;
-    double b = 1024;
-
-    double c = marginFloor(a/b);
-
-    cout << c * b << endl;
-    */
 }
